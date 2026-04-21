e83dc36402d73fa6f9b6daaf24828dfaefee0c56
Janne Enberg <janne.enberg@lietu.net>
2026-04-21 15:33:31 +0300

    Updated website structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import '@fontsource-variable/merriweather-sans';
+
+  // Import global styles
+  import "$styles/init.scss"
+
+  let {children} = $props()
+</script>
+
+<svelte:head>
+  <!-- Primary Meta Tags -->
+  <title>Lietu.net</title>
+  <meta name="title" content="Lietu.net" />
+  <meta name="description" content='Lietu.net' />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://lietu.net/" />
+  <meta property="og:title" content="Lietu.net" />
+  <meta property="og:description" content='Lietu.net' />
+</svelte:head>
+
+<main>
+  {@render children()}
+</main>
+
+<footer>
+  <p>Copyright &copy; Janne Kalevi Enberg, 2026</p>
+  <p>This website does not use cookies, and fuck anyone who claims that cookie dialogs are necessary to have a normally functioning website. They are only necessary for anyone who wants to steal/sell your personal data for profit.</p>
+</footer>
+
+<style lang="scss">
+  :global(html, body), root {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    background-color: #08101a;
+  }
+
+  $padding: 1rem;
+
+  main, footer {
+    padding: $padding;
+    flex-grow: 1;
+    max-width: 40rem;
+    margin: auto;
+  }
+
+  footer {
+    text-align: center;
+    margin-top: 4rem;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<h1>Lietu.net</h1>
+<p>WIP</p>
+
+<style lang="scss">
+  h1 {
+    margin-top: 6rem;
+    font-size: 64px;
+  }
+
+  p {
+    font-weight: 300;
+  }
+</style>
